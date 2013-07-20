56e3f71723395e89187cbc59c6462a3c71acc765
Daniels Umanovskis <daniels@umanovskis.se>
2013-07-20 22:21:36 +0200

    License update

--- src/zkd/solve.cpp
+++ src/zkd/solve.cpp
@@ -1,3 +1,33 @@
+//  Copyright (C) 2013 Brax Sisco, Wesley Hopper, Michael Eaton,
+//	Daniels Umanovskis
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+/************************************************************
+ * This program is a lightweight, command-line hacked
+ * derivation of the 2010 version of ZKDecrypto.
+ *
+ * The use of the GNU GPL for this program does not imply
+ * support for the Free Software Foundation or that
+ * the authors are in any way related to the Free Software
+ * Foundation
+ *
+ ************************************************************/
+
+
 #include "headers/solve.h"
 
 extern SOLVEINFO siSolveInfo;
